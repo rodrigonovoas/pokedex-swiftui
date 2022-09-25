8785806bf0d953c6f1c921ff50393e11ded4cf6e
Rodri <rodrigons@Mac-mini-de-Rodrigo.local>
2022-09-25 16:38:35 +0200

    base swiftui setup

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Rodrigo Novoa Salgado on 25/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
